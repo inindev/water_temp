f4ca8e9b791a577c859fdf6c46f47c4b74766df0
John Clark <inindev@gmail.com>
2019-06-30 07:48:00 -0400

    refactor cleanup

--- led_display/disply_temp.py
+++ led_display/disply_temp.py
@@ -1,3 +1,12 @@
+#
+# date_convert.py
+#
+#   utility for reading the last temperature log
+#   entry and displaying it on a TM1637 LED display
+#   rpi pins gpio_clk=17, gpio_dio=18
+#
+#   John Clark, 2019
+#
 
 from TM1637 import TM1637
 import sys
@@ -5,15 +14,28 @@ sys.path.append('..')
 import my_config
 
 
-with open(my_config.LOG_PATH_LAST, 'r') as fp:
-    ll = list(fp)[-1]
+def get_last_entry(file):
+    last = None
+    with open(file, 'r') as fp:
+        last = list(fp)[-1]
+    return last
 
-vals = ll.split("\t")
-temp_c1000 = int(vals[1])
-temp_f = float(temp_c1000) * 9.0 / 5000.0 + 32.0
 
-disp = TM1637(gpio_clk=17, gpio_dio=18)
-str = '{:.1f}'.format(temp_f)
-print(str)
-disp.set_string(str)
+def c1000_to_fahrenheit(temp_c1000):
+    temp_fahrenheit = float(temp_c1000) * 9.0 / 5000.0 + 32.0
+    return temp_fahrenheit
+
+
+def main():
+    entry = get_last_entry(my_config.LOG_PATH_LAST)
+    temp = c1000_to_fahrenheit(entry[1])
+    temp_str = '{:.1f}'.format(temp)
+
+    disp = TM1637(gpio_clk=17, gpio_dio=18)
+    disp.set_string(temp_str)
+    print(temp_str)
+
+
+if __name__ == '__main__':
+    main()
 
