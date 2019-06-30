d299f9fd17d6a186563fc73a350dec38fc3ff537
John Clark <inindev@gmail.com>
2019-06-30 07:08:31 -0400

    utility for converting date string to unix epoch

---
+++ date_convert.py
@@ -0,0 +1,24 @@
+
+from datetime import datetime, timezone
+import sys
+
+
+# convert date string as epoch date: 2019-06-30 10:58:12 -> 1561892292
+def datestr_to_epoch(datestr):
+	utc = datetime.strptime(datestr, '%Y-%m-%d %H:%M:%S').replace(tzinfo=timezone.utc)
+	utc_epoch = int(utc.timestamp())
+	return utc_epoch
+
+
+def main(argv):
+	if len(argv) != 2:
+		print('usage: python3 {} "YYYY-MM-DD HH:MM:SS"\n'.format(argv[0]))
+
+	datestr = argv[1]
+	epoch = datestr_to_epoch(datestr)
+	print('{} -> {}'.format(datestr, epoch))
+
+
+
+if __name__ == '__main__':
+	main(sys.argv)
