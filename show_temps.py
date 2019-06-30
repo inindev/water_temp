ab3fefe1eca75aa4475dde600421b9d84e5b67e4
John Clark <inindev@gmail.com>
2019-06-30 07:30:30 -0400

    cleanup refactor

--- show_temps.py
+++ show_temps.py
@@ -1,3 +1,11 @@
+#
+# show_temps.py
+#
+#   python utility for displaying the previous 100
+#   temperature readings in human readable format
+#
+#   John Clark, 2019
+#
 
 from datetime import datetime, timezone, timedelta
 import os
@@ -7,24 +15,45 @@ import my_config
 tz_offs = timedelta(hours=4 if os.popen('TZ="US/Eastern" date +%z').read().strip()=='-0400' else 5)
 
 
-with open(my_config.LOG_PATH, 'r') as fp:
-    ll = list(fp)
+def read_file(file):
+    arr = None
+    with open(file, 'r') as fp:
+        arr = list(fp)
+    return arr
 
-n = len(ll)
-n100 = max(0, n - 100)
 
-print('showing reading nums: {} -> {}'.format(n100, n))
-for i in range(n100, n):
-    tokens = ll[i].split('\t')
-
-    utc = datetime.strptime(tokens[0], '%Y-%m-%d %H:%M:%S').replace(tzinfo=timezone.utc)
+def datestr_to_eastern(datestr):
+    utc = datetime.strptime(datestr, '%Y-%m-%d %H:%M:%S').replace(tzinfo=timezone.utc)
     local = utc - tz_offs
     ap = 'p' if local.strftime('%p') == 'PM' else 'a'
-#    date = local.strftime('%I:%M{} %-m/%-d/%Y').format(ap)
-    date = local.strftime('%-m/%-d/%Y %I:%M{}').format(ap)
+    eastern = local.strftime('%-m/%-d/%Y %I:%M{}').format(ap)
+    return eastern
 
-    temp_c1000 = float(tokens[1])
-    temp_f = temp_c1000 * 9.0 / 5000.0 + 32.0
 
-    print('{} -> {:.1f}'.format(date, temp_f))
+def c1000_to_fahrenheit(temp_c1000):
+    temp_fahrenheit = float(temp_c1000) * 9.0 / 5000.0 + 32.0
+    return temp_fahrenheit
+
+
+def entry_to_disp(entry):
+    tokens = entry.split('\t')
+    date = datestr_to_eastern(tokens[0])
+    temp = c1000_to_fahrenheit(tokens[1])
+    disp = '{} -> {:.1f}'.format(date, temp)
+    return disp
+
+
+def main():
+    arr = read_file(my_config.LOG_PATH)
+    amax = len(arr)
+    amin = max(0, amax - 100)
+
+    for i in range(amin, amax):
+        disp = entry_to_disp(arr[i])
+        print(disp)
+
+
+
+if __name__ == '__main__':
+    main()
 
