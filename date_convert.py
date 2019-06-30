2d26a1f2a06c6b942bc0e36aec7d71c7c2921d9d
John Clark <inindev@gmail.com>
2019-06-30 07:13:04 -0400

    formatting

--- date_convert.py
+++ date_convert.py
@@ -5,20 +5,20 @@ import sys
 
 # convert date string as epoch date: 2019-06-30 10:58:12 -> 1561892292
 def datestr_to_epoch(datestr):
-	utc = datetime.strptime(datestr, '%Y-%m-%d %H:%M:%S').replace(tzinfo=timezone.utc)
-	utc_epoch = int(utc.timestamp())
-	return utc_epoch
+    utc = datetime.strptime(datestr, '%Y-%m-%d %H:%M:%S').replace(tzinfo=timezone.utc)
+    utc_epoch = int(utc.timestamp())
+    return utc_epoch
 
 
 def main(argv):
-	if len(argv) != 2:
-		print('usage: python3 {} "YYYY-MM-DD HH:MM:SS"\n'.format(argv[0]))
+    if len(argv) != 2:
+        print('usage: python3 {} "YYYY-MM-DD HH:MM:SS"\n'.format(argv[0]))
 
-	datestr = argv[1]
-	epoch = datestr_to_epoch(datestr)
-	print('{} -> {}'.format(datestr, epoch))
+    datestr = argv[1]
+    epoch = datestr_to_epoch(datestr)
+    print('{} -> {}'.format(datestr, epoch))
 
 
 
 if __name__ == '__main__':
-	main(sys.argv)
+    main(sys.argv)
