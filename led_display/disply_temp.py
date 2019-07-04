7d44b250d35b868fadad0ea602834ef564279c24
John Clark <inindev@gmail.com>
2019-07-04 15:16:40 -0400

    suppress output during blackout hours

--- led_display/disply_temp.py
+++ led_display/disply_temp.py
@@ -9,6 +9,7 @@
 #
 
 from TM1637 import TM1637
+from datetime import datetime
 import sys
 import my_config
 
@@ -25,15 +26,36 @@ def c1000_to_fahrenheit(temp_c1000):
     return temp_f10 / 10.0
 
 
+def can_show():
+    dtnow = datetime.utcnow().replace(microsecond=0)
+
+    # date_begin: april -> 4/1
+    # date_end: nov     -> 11/1
+    can_show_month = (dtnow.month > 3) and (dtnow.month < 11)
+
+    # time begin: 7:00a -> 11:00 UTC
+    # time end: 6:00p   -> 22:00 UTC
+    can_show_hour = (dtnow.hour > 10) and (dtnow.hour < 22)
+
+    return can_show_month and can_show_hour
+
+
 def main():
+    disp = TM1637(gpio_clk=17, gpio_dio=18)
+
+    if not can_show():
+        disp.brightness = 0;
+        print('blackout time, not showing...')
+        return
+
     entry = get_last_entry(my_config.path.log)
     tokens = entry.split('\t')
     temp = c1000_to_fahrenheit(tokens[1])
     temp_str = '{:.1f}'.format(temp)
 
-    disp = TM1637(gpio_clk=17, gpio_dio=18)
     disp.set_string(temp_str)
     print(temp_str)
+
 
 
 if __name__ == '__main__':
